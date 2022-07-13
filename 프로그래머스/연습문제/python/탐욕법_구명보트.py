f63ad8c0e712c180a9b8699bc9ed420280e78827
yeafla530 <yeafla530@naver.com>
2022-07-13 23:30:12 +0900

    신고결과받기

--- 프로그래머스/연습문제/python/탐욕법_구명보트.py
+++ 프로그래머스/연습문제/python/탐욕법_구명보트.py
@@ -26,3 +26,28 @@ def solution(people, limit):
             answer += 1
         # print(people)
     return answer
+
+
+# 내가 실수한 점 
+# [:] slice도 O(n)의 시간복잡도를 가짐
+def solution(people, limit):
+    # 한번에 최대 2명
+    # 무게 제한
+    # 구명보트 최대한 적게
+    # 모든 사람을 구하기 위한 구명보트 개수 최솟값
+    answer = 0
+    start = 0
+    end = len(people) - 1
+    
+    people.sort()
+    # start와 end 가 만날때까지
+    while start <= end:
+        answer += 1
+        # 2명을 태울 수있으면
+        if (people[start] + people[end] <= limit):
+            # 태우고 다음 사람
+            start += 1
+        # 무거운 순으로 태움
+        end -= 1
+        
+    return answer
---
+++ 프로그래머스/연습문제/신고결과받기.js
@@ -0,0 +1,59 @@
+// 내 풀이
+function solution(id_list, report, k) {
+    // 한번에 한명 유저 신고 가능
+    // 동일한 유저에 대한 신고는 1번만 가능
+    // k번 이상 신고된 유저는 게시판 이용이 정지됨 => 해당 유저를 신고한 모든 유저에게 정지 사실을 메일로 발송
+    // 마지막에 한꺼번에 게시판 이용 정지시켜 정지 메일 발송
+    let answer = [];
+    let countDict = {}
+    let reported = new Object
+    // console.log(report)
+    
+    for (let i = 0 ; i < id_list.length; i++) {
+        countDict[id_list[i]] = 0
+    }
+    // console.log(countDict)
+    
+    for (const word of report) {
+        let name = word.split(' ')
+        
+        if (reported[name[1]] && reported[name[1]].includes(name[0])) {
+            continue
+        } else {
+            reported[name[1]] ? reported[name[1]].push(name[0]) : reported[name[1]] = [name[0]]   
+        }
+    }
+    
+    for (const [key, val] of Object.entries(reported)) {
+        if (val.length >= k) {
+            // console.log('확인')
+            for (let item of val) {
+                countDict[item] += 1
+            }
+        }
+    }
+    
+    answer = Object.values(countDict)
+
+            
+    
+    // console.log(answer)
+    return answer;
+}
+
+// 다른 사람 풀이
+function solution(id_list, report, k) {
+    let reports = [...new Set(report)].map(a=>{return a.split(' ')});
+    let counts = new Map();
+    for (const bad of reports){
+        counts.set(bad[1],counts.get(bad[1])+1||1)
+    }
+    let good = new Map();
+    for(const report of reports){
+        if(counts.get(report[1])>=k){
+            good.set(report[0],good.get(report[0])+1||1)
+        }
+    }
+    let answer = id_list.map(a=>good.get(a)||0)
+    return answer;
+}
