a1ddc7d9ae4e3a7940de25872c713ea5cb690094
yeafla530 <yeafla530@naver.com>
2022-08-09 11:15:15 +0900

    n개 숫자중 최대2개

--- 코드트리/NL/n개숫자중최대2개.py
+++ 코드트리/NL/n개숫자중최대2개.py
@@ -22,3 +22,29 @@ for i in range(n):
         max2 = arr[i]
 
 print(max1, max2)
+
+
+
+# 두번째 풀이
+# N개의 정수를 내림차순으로 정렬
+n = int(input())
+arr = list(map(int, input().split()))
+
+if arr[0] > arr[1]:
+    max1, max2 = arr[0], arr[1]
+
+else:
+    max2, max1 = arr[0], arr[1]
+
+
+for i in range(2, n):
+    # Case 1 : 지금까지 본 숫자들보다 좋다면
+        #          max2, max1 모두 갱신해줍니다.
+    if arr[i] >= max1:
+        max2, max1 = max1, arr[i]
+        
+     # Case 2 : max2보다만 좋다면 max2를 갱신합니다.
+    elif arr[i] > max2:
+        max2 = arr[i]
+
+print(max1, max2)
