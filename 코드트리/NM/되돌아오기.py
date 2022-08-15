2184159f66ad77e7394a725e6bf813ada33110f3
yeafla530 <yeafla530@naver.com>
2022-08-15 20:37:06 +0900

    되돌아오기 함수이용

--- 코드트리/NM/되돌아오기.py
+++ 코드트리/NM/되돌아오기.py
@@ -38,3 +38,47 @@ if is_research:
     print(t)
 else:
     print(-1)
+
+# 정답풀이
+n = int(input())
+d = {
+    'E': 0,
+    'S': 1,
+    'W': 2,
+    'N': 3
+}
+
+dxs = [0, 1, 0, -1]
+dys = [1, 0, -1, 0]
+
+
+x, y = 0, 0
+cur_dir = 0
+ans = -1
+elapsed_time = 0
+
+def move(move_dir, dist):
+    global x, y
+    global ans, elapsed_time
+
+    for _ in range(dist):
+        x, y = x + dxs[cur_dir], y + dys[cur_dir]
+        elapsed_time += 1
+
+        if x == 0 and y == 0:
+            ans = elapsed_time 
+            return True
+    return False
+
+for _ in range(n):
+    dis, a = input().split()
+    a = int(a)
+    cur_dir = d[dis]
+
+
+    done = move(dis, a)
+
+    if done:
+        break
+           
+print(ans)
