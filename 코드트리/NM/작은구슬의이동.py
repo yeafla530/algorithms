c8af7cd90eafa3038074b1e030ed0f5e0a5613e6
yeafla530 <yeafla530@naver.com>
2022-08-15 22:23:13 +0900

    문자에따른명령2, 작은구슬의 이동 수저

--- 코드트리/NM/작은구슬의이동.py
+++ 코드트리/NM/작은구슬의이동.py
@@ -27,3 +27,37 @@ for _ in range(t):
 print(r+1, c+1)
     
 
+## 강사님과 푼 풀이
+n, t = map(int, input().split())
+x, y, c_dir = input().split()
+x, y = int(x)-1, int(y)-1
+
+mapper = {
+    'R': 0,
+    'D': 1,
+    'U': 2,
+    'L': 3
+}
+
+move_dir = mapper[c_dir]
+
+dxs = [0, 1, -1, 0]
+dys = [1, 0, 0, -1]
+
+# 격자 위에 있는가
+def in_range(x, y):
+    return 0<=x<n and 0<=y<n
+
+for _ in range(t):
+    # 미리보기
+    nx, ny = x + dxs[move_dir], y + dys[move_dir]
+
+    # 격자 위에?
+    if in_range(nx, ny):
+        x, y = nx, ny
+    
+    # 벽에 부딪치면
+    else:
+        move_dir = 3 - move_dir
+
+print(x+1, y+1)
