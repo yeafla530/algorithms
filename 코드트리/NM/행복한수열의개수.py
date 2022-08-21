8ccf886ef15d060d965c595f7d83b914a5052747
yeafla530 <yeafla530@naver.com>
2022-08-22 01:10:00 +0900

    행복한수열의 개수

--- 코드트리/NM/행복한수열의개수.py
+++ 코드트리/NM/행복한수열의개수.py
@@ -1,1 +1,35 @@
-ddddd
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+seq = [0 for _ in range(n)]
+
+# 행 세기
+def is_happy_seq():
+    cnt = 1
+    max_cnt = 1 
+    for i in range(n-1):
+        if seq[i] == seq[i+1]:
+            cnt += 1
+        else:
+            cnt = 1
+        
+        max_cnt = max(max_cnt, cnt)
+    
+    return max_cnt >= m
+
+
+# 행
+ans = 0
+for i in range(n):
+    seq = arr[i][:]
+
+    if is_happy_seq():
+        ans += 1
+
+for j in range(n):
+    for i in range(n):
+        seq[i] = arr[i][j]
+    if is_happy_seq():
+        ans += 1
+
+print(ans)
+
