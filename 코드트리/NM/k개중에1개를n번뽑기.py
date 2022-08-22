db0157770fe72d3a24cc1aa5145a2457851e8980
yeafla530 <yeafla530@naver.com>
2022-08-22 21:11:16 +0900

    k개 중에 1개를 n번뽑기

---
+++ 코드트리/NM/k개중에1개를n번뽑기.py
@@ -0,0 +1,21 @@
+k, n = map(int, input().split())
+arr = []
+
+# cnt번째 자리 숫자 결정
+def select(a):
+    # 종료조건
+    # 횟수n 넘으면
+    if a == n+1:
+        print(*arr)
+        return
+    # 재귀호출
+    # 1부터 시작해서 사전순으로 정렬됨
+    for i in range(1, k+1):
+        # i를 위한 연산
+        arr.append(i)
+        select(a+1)
+        # i에 대한 역 연산
+        arr.pop()
+
+
+select(1)
