27a893741a644bf0dc88409e6751792be1c5c8f8
yeafla530 <yeafla530@naver.com>
2022-08-31 00:43:41 +0900

    마을 구분하기

--- 코드트리/IL/DFS/마을구분하기.py
+++ 코드트리/IL/DFS/마을구분하기.py
@@ -4,15 +4,37 @@ visited = [[0]*n for _ in range(n)]
 dxs = [0, 1, 0, -1]
 dys = [1, 0, -1, 0]
 
-def dfs(x, y, cnt):
+def can_go(x, y):
+    if not (0 <= x < n and 0 <= y < n):
+        return False
+    if villiage[x][y] == 0 or visited[x][y]:
+        return False
+    
+    return True
+     
+# 연결된 사람 찾기
+def dfs(x, y):
+    global people_num
     for dx,dy in zip(dxs, dys):
         nx = x + dx
         ny = y + dy
 
         if can_go(nx, ny):
-            cnt += 1
+            visited[nx][ny] = 1
+            people_num += 1
+            dfs(nx, ny)
     
-    return cnt
 
-
-dfs(0, 0, 0)
+people = []
+for i in range(n):
+    for j in range(n):
+        if can_go(i, j):
+            visited[i][j] = 1
+            people_num = 1
+            dfs(i, j)
+            people.append(people_num)
+    
+people.sort()
+print(len(people))
+for i in people:
+    print(i)
