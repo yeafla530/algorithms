6aa770cfe3774477880e34081b26c7c5d78cd65b
yeafla530 <yeafla530@naver.com>
2022-09-08 00:00:52 +0900

    어려움 - 적절한 옷 고르기

--- 코드트리/IL/DP/다시_적절한옷고르기.py
+++ 코드트리/IL/DP/다시_적절한옷고르기.py
@@ -1,40 +1,38 @@
+import sys
+INT_MIN = -sys.maxsize
 # n = 옷수, m = 일수
 n, m = map(int, input().split())
-a = [[0]*(m+1) for _ in range(n+1)]
-dp = [[0]*(m+1) for _ in range(n+1)]
+s = [0] * (n+1)
+e = [0] * (n+1)
+v = [0] * (n+1)
 
-for i in range(1, n+1):
-    s, e, v = map(int, input().split())
-    for j in range(s, e+1):
-        a[i][j] = v
-# print(a)
-# initial
-for i in range(1, n+1):
-    dp[i][1] = 0
+# dp[i][j] : i번째 날까지 입을 옷 전부 결정했고, 마지막 날에 입은 옷이 j옷이라고 했을 때, 얻을 수 있는 최대 만족도
+dp = [[0]*(n+1) for _ in range(m+1)]
 
+def initialize():
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            dp[i][j] = INT_MIN
 
-# print(a)
-
-# 일수는 고정
-for i in range(2, m+1):
-    # j = i일수에 선택한 옷
     for j in range(1, n+1):
-        # k = 그 전에 고른 옷의 번호
-        for k in range(1, n+1):
-            # # 지금 선택한 옷의 화려함, 그 전에 선택한 옷의 화려함
-            if a[j][i] and a[k][i-1] and a[k][i]:
-                # 화려함의 차이를 더한 최대값
-                # print(i)
-                dp[j][i] = max(dp[j][i], dp[k][i-1] + abs(a[k][i-1] - a[j][i]))
-                print(j, i, k)
-                print(dp[j][i])
-                print(dp[k][i-1] + abs(a[k][i] - a[j][i]))
-                print()
-print(dp)
-max_num = 0
+        if s[j] == 1:
+            dp[1][j] = 0
+
+
 for i in range(1, n+1):
-    max_num = max(dp[i][m], max_num)
+    s[i], e[i], v[i] = tuple(map(int, input().split()))
 
-print(max_num)
+initialize()
 
-            
+for i in range(2, m+1):
+    for j in range(1, n+1):
+        for k in range(1, n+1):
+            # i-1번째 날에 k번 옷을 입은 경우 고려
+            # 단 k번 옷이 i-1번째 날에 입을 수 있어야하고 j번 옷이 i번째 날에 입을 수 있는 경우에만 고려
+            # j번 옷이 i번째 날에 입을 수 있는 경우에만 고려해볼 수 있음
+            if s[k] <= i-1 <= e[k] and s[j] <= i <= e[j]:
+                dp[i][j] = max(dp[i][j], dp[i-1][k] + abs(v[j] - v[k]))
+
+
+ans = max(dp[m][1:n+1])
+print(ans)
