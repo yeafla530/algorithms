521ac49faaf2736aa369d35c8168d7ce0139e5b8
yeafla530 <yeafla530@naver.com>
2022-09-15 01:45:18 +0900

    1차원 폭발 게임

--- 코드트리/IL/시뮬레이션/1차원폭발게임.py
+++ 코드트리/IL/시뮬레이션/1차원폭발게임.py
@@ -1,61 +1,36 @@
 n, m = map(int, input().split())
-bomb = [int(input()) for _ in range(n)]
-box = []
-is_posibble = False
+numbers = [int(input()) for _ in range(n)]
 
-# 더이상 폭탄이 터지지 않을때까지 반복
+start_idx = 0
+def get_end_idx(number, start):
+    for end in range(start+1, len(numbers)):
+        if numbers[end] != number:
+            return end - 1
+    return len(numbers) - 1
+
 while True:
-    # 모든 폭탄 번호가 같은 경우에는?
-    temp = bomb[0]
-    s, e = 0, 0
-    cnt = 1
-    for i in range(1, len(bomb)):
-        print(bomb[i], temp)
-        # 이전 번호와 현재 번호가 같은 경우
-        if temp == bomb[i]:
-            cnt += 1
-        # end point + 1 
-        # 개수 + 1
+    did_boom = False
 
-        # 이전 번호와 현재 번호가 다른 경우 
-        else:
-            e = i
-            # 개수가 m보다 크거나 같은 경우 => 
-            if cnt >= m:
-                # print(cnt, m)
-                # s부터 e까지 boom
-                for j in range(s, e):
-                    bomb[j] = 0
-                # s, e, n 초기화
-            s = i
-            cnt = 1
-            temp = bomb[i]
-            # 개수가 m보다 작은경우 => 유지
-    # print(bomb)
-    e = len(bomb)
-    # 마지막에 boom check (s, e까지)
-    if cnt >= m:
-        for j in range(s, e):
-            bomb[j] = 0
+    for curr_idx, number in enumerate(numbers):
+        if number == 0:
+            continue
+        
+        end_idx = get_end_idx(number, curr_idx)
+        
+
+        if end_idx - curr_idx + 1 >= m:
+            numbers[curr_idx:end_idx+1] = [0] * (end_idx - curr_idx)
+            # print(numbers)
+            did_boom = True
+            curr_idx = end_idx+1
     
-    for i in range(len(bomb)):
-        if bomb[i]:
-            box.append(bomb[i])
-    
-    
-    bomb = box[:]
+    numbers = list(filter(lambda x: x > 0, numbers))
 
-
-    # 더이상 폭탄이 터질 수 없으면 break
-    for i in range(len(bomb)-m+1):
-        # 더이상 폭탄이 터질수 없는지 확인 
-        if bomb[i:i+m].count(bomb[i]) >= m:
-            is_posibble = True
-
-    if not is_posibble:
+    if not did_boom:
         break
+    
+print(len(numbers))
+for number in numbers:
+    print(number)
 
-print(len(bomb))
-if len(bomb):
-    for i in range(len(bomb)):
-        print(bomb[i])
+
