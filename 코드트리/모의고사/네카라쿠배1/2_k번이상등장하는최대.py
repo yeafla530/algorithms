4067f09006a04935588338f9795a8a6faaf853bd
yeafla530 <yeafla530@naver.com>
2022-09-16 03:11:25 +0900

    k번 이상 등장하는 최대

--- 코드트리/모의고사/네카라쿠배1/2_k번이상등장하는최대.py
+++ 코드트리/모의고사/네카라쿠배1/2_k번이상등장하는최대.py
@@ -38,49 +38,28 @@ if e-s+1 >= k:
     if cur_num > max_num:
         max_num = cur_num
 
-##### 2중 for문 풀이 => 시간 초과 
-### 값을 기준으로 완전탐색 문제 공부하기
-# 시간복잡도 O(N^2)
-# 1억 = 1초
-# 10만 제곱 = 100억 => 시간초과
-
-
-
-##### hash map 풀이
-# for 문을 한번 더 도는것은 비효율적
-# key-value 사용
-# IM의 HashMap 풀기
-# 전처리 진행 : Preprocessing
-freq = {}
-# print(freq[4]) # error
-
-for elem in arr:
-    # 우리가 기대하는 값 안나옴 
-    freq[elem] = freq[elem] + 1
-
-    # 처음엔 이렇게 많이 작성
-    if elem in freq:
-        freq[elem] = freq[elem] + 1
-    else:
-        freq[elem] = 1
-
-    # get 사용
-    # get(key, default) => key값이 비어있으면 default로 반환해줌
-    freq[elem] = freq.get(elem, 0) + 1
-
-# 해결법 1: defaultdict 사용하면 
-from collections import defaultdict
-freq = defaultdict(lambda: 0) # 비어있으면 0으로 초기화 해준다는 뜻
-
-
-###### count 배열처럼 하면 메모리 초과 => 10의 9승이기 때문에
-###### 그래서 HashMap
-
-# 배열크기가 2000만 넘으면 잘못 생각한것임
-
-
-
 print(max_num)
+
+
+### hashmap 풀이
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+freq = {}
+
+
+
+
+for i in range(n):
+    freq[a[i]] = freq.get(a[i], 0) + 1
+
+result = -1
+for key in freq.keys():
+    # print(key, value)
+    if freq[key] >= k:
+        result = max(result, key)
+
+print(result)
+
 
 
 
--- 코드트리/모의고사/네카라쿠배1/풀이2_k번이상등장하는최대.py
+++ 코드트리/모의고사/네카라쿠배1/풀이2_k번이상등장하는최대.py
@@ -27,3 +27,44 @@ for elem in arr:
         ans = max(ans, elem)
 
 print(ans)
+
+
+##### 2중 for문 풀이 => 시간 초과 
+### 값을 기준으로 완전탐색 문제 공부하기
+# 시간복잡도 O(N^2)
+# 1억 = 1초
+# 10만 제곱 = 100억 => 시간초과
+
+
+
+##### hash map 풀이
+# for 문을 한번 더 도는것은 비효율적
+# key-value 사용
+# IM의 HashMap 풀기
+# 전처리 진행 : Preprocessing
+freq = {}
+# print(freq[4]) # error
+
+for elem in arr:
+    # 우리가 기대하는 값 안나옴 
+    freq[elem] = freq[elem] + 1
+
+    # 처음엔 이렇게 많이 작성
+    if elem in freq:
+        freq[elem] = freq[elem] + 1
+    else:
+        freq[elem] = 1
+
+    # get 사용
+    # get(key, default) => key값이 비어있으면 default로 반환해줌
+    freq[elem] = freq.get(elem, 0) + 1
+
+# 해결법 1: defaultdict 사용하면 
+from collections import defaultdict
+freq = defaultdict(lambda: 0) # 비어있으면 0으로 초기화 해준다는 뜻
+
+
+###### count 배열처럼 하면 메모리 초과 => 10의 9승이기 때문에
+###### 그래서 HashMap
+
+# 배열크기가 2000만 넘으면 잘못 생각한것임
