c271b7ea6ff9bfeb6fe40d07a91da06d5d452ab2
yeafla530 <yeafla530@naver.com>
2022-09-22 01:14:22 +0900

    정수 n개의 합3

--- 코드트리/IM/Shorten_time_Technique/PrefixSum/정수n개의합3.py
+++ 코드트리/IM/Shorten_time_Technique/PrefixSum/정수n개의합3.py
@@ -1,7 +1,22 @@
 n, k = map(int, input().split())
-arr = [[0] * (n + 1)] + [
-    [0] + list(map(int, input().split()))
-    for _ in range(n)
-]
-prefix_sum = [[0]*(n+1) for _ in range(n+1)]
+arr = [[0] * (n+1)] + [[0] + list(map(int, input().split())) for _ in range(n)]
+
+prefix = [[0]*(n+1) for _ in range(n+1)]
+
+def get_sum(x1, x2, y1, y2):
+    return prefix[x2][y2] - prefix[x1-1][y2] - prefix[x2][y1-1] + prefix[x1-1][y1-1]
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1] + arr[i][j]
+    
+# print(prefix)
+
+result = 0
+for i in range(1, n-k+2):
+    for j in range(1, n-k+2):
+        # print(get_sum(i, i+k-1, j, j+k-1))
+        result = max(result, get_sum(i, i+k-1, j, j+k-1))
+
+print(result)
 
