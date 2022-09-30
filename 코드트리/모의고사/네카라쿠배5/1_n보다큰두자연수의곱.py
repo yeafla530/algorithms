4ed47a605949f6529af44ad9bc3a92d666fb254c
yeafla530 <yeafla530@naver.com>
2022-09-30 22:03:20 +0900

    n보다 큰 두 자연수의 곱

--- 코드트리/모의고사/네카라쿠배5/1_n보다큰두자연수의곱.py
+++ 코드트리/모의고사/네카라쿠배5/1_n보다큰두자연수의곱.py
@@ -10,3 +10,23 @@ for i in range(1, INT+1):
 
 print(ans)
 
+### 완탐 최적화
+from math import sqrt
+import sys
+
+INT_MAX = sys.maxsize
+n, x = map(int, input().split())
+ans = INT_MAX
+
+sq = int(sqrt(n))
+# n의 제곱근 + 1 까지
+for a in range(1, sq+2):
+    # a * b > n => b > n // a => b의 최소: n//a+1
+    b = max(a, n // a + 1)
+    if b - a <= x:
+        ans = min(ans, a * b)
+
+
+
+print(ans)
+
