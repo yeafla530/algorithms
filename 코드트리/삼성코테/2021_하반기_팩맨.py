a0445b7e520eeea8c28330aa7b3df3787aee71e3
yeafla530 <yeafla530@naver.com>
2022-10-12 20:59:13 +0900

    팩맨

--- 코드트리/삼성코테/2021_하반기_팩맨.py
+++ 코드트리/삼성코테/2021_하반기_팩맨.py
@@ -11,7 +11,7 @@ result = 0
 packman_graph[r][c] = 1
 for _ in range(m):
     x, y, d = map(lambda x: int(x) - 1, input().split())
-    graph[x][y].append((2, d))
+    graph[x][y].append(d)
 
 
 # 1. 몬스터 복제 시도
@@ -20,8 +20,10 @@ def try_monster_copy():
     for i in range(n):
         for j in range(n):
             if len(graph[i][j]):
-                for num, d in graph[i][j]:
-                    egg_graph.append(d)
+                for d in graph[i][j]:
+                    egg_graph[i][j].append(d)
+
+    # print(egg_graph)
 
 
 def in_range(x, y):
@@ -47,15 +49,21 @@ def move_monster():
     for i in range(n):
         for j in range(n):
             next_grahp[i][j] = []
+
     # 자신이 가진 방향으로 이동
     for i in range(n):
         for j in range(n):
             if len(graph[i][j]):
-                for num, d in graph[i][j]:
+                for d in graph[i][j]:
+                    # visit 초기화
                     for x in range(8):
                         visit_dir[x] = 0
-
+                    select_x = 0
+                    select_y = 0
                     is_move = False
+
+                    # 해당 방향으로 갈 수 없다면 될때까지 반복
+                    # 모두 움직일 수 없으면 움직이지 않기
                     while True:
                         # 이미 방문했던 위치나 움직인 경우
                         if is_move or visit_dir[d]:
@@ -77,94 +85,185 @@ def move_monster():
                             continue
 
                         is_move = True
-                        # 해당 방향으로 갈 수 없다면 될때까지 반복
-                        # 모두 움직일 수 없으면 움직이지 않기
+                        select_x = nx
+                        select_y = ny
 
+                    next_grahp[select_x][select_y].append(d)
+
+    for i in range(n):
+        for j in range(n):
+            graph[i][j] = next_grahp[i][j]
+
+
+visit_graph = [[0]*n for _ in range(n)]
 def calc_score(x, y, dir_arr):
+    # 상좌하우
     dxs = [-1, 0, 1, 0]
     dys = [0, -1, 0, 1]
     nx = x
     ny = y
+    # print(x, y)
     value = 0
+
+    for i in range(n):
+        for j in range(n):
+            visit_graph[i][j] = 0
 
     for di in dir_arr:
         nx = nx + dxs[di]
         ny = ny + dys[di]
+
         if not in_range(nx, ny):
-            return 0
+            return -1
+        if visit_graph[nx][ny]:
+            return -1
+
         if len(graph[nx][ny]):
             value += len(graph[nx][ny])
-
-    print(value)
+            visit_graph[nx][ny] = 1
     return value
 
 
 
 # 팩맨 이동 담기
-dir_arr = []
-result_dir_arr = []
-max_score = 0
+
+def choose(cnt, x, y):
+    global max_score, result_dir_arr
+    if cnt == 3:
+        score = calc_score(x, y, dir_arr)
+        # 해당 경로가 최대 경로면
+        # 원래 위치에서 이동
+        if max_score < score:
+            # print(dir_arr, score)
+            max_score = score
+            result_dir_arr = dir_arr[:]
+        return
+
+    for i in range(4):
+        dir_arr.append(i)
+        choose(cnt+1, x, y)
+        dir_arr.pop()
+
+
+
 # 3. 팩맨이동
 def move_packman():
+    global result, max_score
     # 상하좌우로만 움직일 수 있음 (3칸)
-
+    dxs = [-1, 0, 1, 0]
+    dys = [0, -1, 0, 1]
+    max_score = 0
     # 팩맨 위치 찾기
     for i in range(n):
         for j in range(n):
             if packman_graph[i][j]:
-                x = i
-                y = j
-
+                pack_x = i
+                pack_y = j
     # 몬스터 가장 많이 먹을 수 있는 방향으로 이동
     # 상 좌 하 우 우선순위
-    # 테스트용
-    dir_arr = []
-    result_dir_arr = []
-    # 최대 경로 담아줌
-
-    def choose(cnt):
-        global max_score, result_dir_arr
-        if cnt == 3:
-            print(dir_arr)
-            score = calc_score(x, y, dir_arr)
-            # 해당 경로가 최대 경로면
-            # 원래 위치에서 이동
-            if max_score < score:
-                max_score = score
-                result_dir_arr = dir_arr
-
-            return
-
-        for i in range(4):
-            dir_arr.append(i)
-            choose(cnt+1)
-            dir_arr.pop()
-
-    choose(0)
-
-    print(result_dir_arr)
+    choose(0, pack_x, pack_y)
+    # print(max_score)
     # 알은 먹지 않는다, 움직이기 전 함께 있던 몬스터도 먹지 않는다
     # 몬스터를 먹는다 => 시체 남김
+    nx = pack_x
+    ny = pack_y
+    # print(result_dir_arr)
+    for d in result_dir_arr:
+        nx = nx + dxs[d]
+        ny = ny + dys[d]
+        # print(nx, ny, graph[nx][ny])
+        if len(graph[nx][ny]):
+            # print(graph[nx][ny][:])
+            for _ in range(len(graph[nx][ny])):
+                dead_graph[nx][ny].append(2)
+                graph[nx][ny] = []
+
+    # print(graph)
+    # 팩맨 위치 변경
+    packman_graph[pack_x][pack_y] = 0
+    packman_graph[nx][ny] = 1
 
 # 4. 시체 소멸
-# def delete_dead():
+def delete_dead():
     # 2턴 후에 사라진다
+    for i in range(n):
+        for j in range(n):
+            if len(dead_graph[i][j]):
+                for x in range(len(dead_graph[i][j])):
+                    if dead_graph[i][j][x] != 0:
+                        dead_graph[i][j][x] -= 1
+                        delete_graph[i][j].append(dead_graph[i][j][x])
+    # print(delete_graph)
+    for i in range(n):
+        for j in range(n):
+            dead_graph[i][j] = delete_graph[i][j]
 
 
 # 5. 복제 완성
-# def complete_copy():
+def complete_copy():
     # 알 형태 몬스터 태어남
+    for i in range(n):
+        for j in range(n):
+            # print(egg_graph[i][j])
+            if len(egg_graph[i][j]):
+                for x in egg_graph[i][j]:
+                    graph[i][j].append(x)
 
+            egg_graph[i][j] = []
 
-# 팩맨 : 1, 몬스터: 2, 알: 3, 시체: 4
 for _ in range(t):
     # 1. 몬스터 복제 시도
     try_monster_copy()
+    print("알")
+    for i in range(n):
+        for j in range(n):
+            print(egg_graph[i][j], end=" ")
+        print()
     # 2. 몬스터 이동
     move_monster()
-    # # 3. 팩맨 이동
+    print("몬스터이동")
+    for i in range(n):
+        for j in range(n):
+            print(graph[i][j], end=" ")
+        print()
+    # 3. 팩맨 이동
+    max_score = 0
+    result_dir_arr = []
+    dir_arr = []
     move_packman()
-    # # 4. 시체 소멸
-    # delete_dead()
-    # # 5. 복제완성
-    # complete_copy()
+    print("팩맨")
+    print(result_dir_arr, max_score)
+    for i in range(n):
+        for j in range(n):
+            print(packman_graph[i][j], end=" ")
+        print()
+
+
+    delete_graph = [[[] for _ in range(n)] for _ in range(n)]
+    # 4. 시체 소멸
+    delete_dead()
+    print("시체")
+    for i in range(n):
+        for j in range(n):
+            print(dead_graph[i][j], end=" ")
+        print()
+
+
+
+    # 5. 복제완성
+    complete_copy()
+    print("복제완성")
+    for i in range(n):
+        for j in range(n):
+            print(graph[i][j], end=" ")
+        print()
+
+
+
+
+# 살아남은 몬스터 계산
+for i in range(n):
+    for j in range(n):
+        result += len(graph[i][j])
+
+print(result)
