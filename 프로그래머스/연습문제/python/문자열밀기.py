ddece3ebd13a1c377039d9cc1dfb58838ea8cecc
yeafla530 <yeafla530@naver.com>
2022-11-29 22:28:24 +0900

    문자열밀기

--- 프로그래머스/연습문제/python/문자열밀기.py
+++ 프로그래머스/연습문제/python/문자열밀기.py
@@ -1,3 +1,16 @@
 def solution(A, B):
-    answer = 0
+    answer = -1
+    count = len(A)
+    s = A
+    
+    for i in range(0, count):
+        if s == B:
+            answer = i
+            break
+        
+        s = A[-1] + A[:-1]
+        A = s
+        # print(s)
+        
+    
     return answer
