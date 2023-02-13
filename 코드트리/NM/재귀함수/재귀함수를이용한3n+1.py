b2c0f9eacf3a2466a11bb3461ed2c538694c60d8
yeafla530 <yeafla530@naver.com>
2023-02-13 17:33:12 +0900

    재귀함수를이용한 3n+1

---
+++ 코드트리/NM/재귀함수/재귀함수를이용한3n+1.py
@@ -0,0 +1,13 @@
+
+n = int(input())
+
+def calc(n):
+    if n == 1:
+        return 0
+    
+    if n % 2 == 0:
+        return calc(n // 2) + 1
+    else:
+        return calc(n * 3 + 1) + 1
+
+print(calc(n))
