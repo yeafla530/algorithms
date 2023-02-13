f0bcdb8a3aab7a6c0d979f268a013642c630de73
yeafla530 <yeafla530@naver.com>
2023-02-13 16:20:50 +0900

    반복 출력하기2

--- 코드트리/NM/함수/특정구간의합.py
+++ 코드트리/NM/함수/특정구간의합.py
@@ -8,3 +8,20 @@ for _ in range(m):
     s = sum(arr[a-1:b])
 
     print(s)
+
+# 정답
+n, m = map(int, input().split())
+arr = [0] + list(map(int, input().split()))
+
+def get_answer(a, b):
+    return_value = 0
+    for i in range(a, b+1):
+        return_value += arr[i]
+    
+    return return_value
+
+
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    print(get_answer(a, b))
