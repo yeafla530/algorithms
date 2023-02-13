835554d8537dadbc8a6f508c43078613bcdd6f58
yeafla530 <yeafla530@naver.com>
2023-02-13 23:18:55 +0900

    연속되는수3

---
+++ 코드트리/NM/시뮬레이션/연속되는수3.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+
+ans, cnt = 0, 0
+for i in range(n):
+    if i > 0 and arr[i] * arr[i-1] > 0:
+        cnt += 1
+    else:
+        cnt = 1
+    
+    ans = max(ans, cnt)
+
+print(ans)
