22ed8890eecb1aa8433a14cccd8ef90cd7709e16
yeafla530 <yeafla530@naver.com>
2023-02-14 18:19:27 +0900

    갈수 있는 곳들:

--- 코드트리/IL/BFS/갈수있는곳들.py
+++ 코드트리/IL/BFS/갈수있는곳들.py
@@ -42,3 +42,54 @@ for i in range(n):
         cnt += visited[i][j]
 
 print(cnt)
+
+
+# 풀이 2
+from collections import deque
+
+
+n, k = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0]*n for _ in range(n)]
+q = deque()
+
+dxs = [1, -1, 0, 0]
+dys = [0, 0, 1, -1]
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+
+def can_go(x, y):
+    return in_range(x, y) and graph[x][y] == 0 and not visited[x][y]
+
+def bfs():
+    global count
+    while len(q):
+        x, y = q.popleft()
+        
+        for dx, dy in zip(dxs, dys):
+            nx = x + dx
+            ny = y + dy
+
+            if can_go(nx, ny):
+                q.append((nx, ny))
+                visited[nx][ny] = 1
+                count += 1
+
+
+
+
+count = 0
+for _ in range(k):
+    r, c = map(int, input().split())
+    r -= 1
+    c -= 1
+
+    if can_go(r, c):
+        visited[r][c] = 1
+        count += 1
+        q.append((r, c))
+        bfs()
+
+print(count)
