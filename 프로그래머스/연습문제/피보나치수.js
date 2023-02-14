12bd6b6c00bbe4054f89bc3e1f1e816d3af7f686
yeafla530 <yeafla530@naver.com>
2023-02-14 21:36:42 +0900

    피보나치 수

---
+++ 프로그래머스/연습문제/피보나치수.js
@@ -0,0 +1,29 @@
+// 내 풀이
+function solution(n) {
+    let answer = 0;
+    let arr = [0, 1]
+    let mino_one = 0
+    let mino_two = 0
+    for (let i = 2; i < n; i++) {
+        mino_one = arr[i-1] % 1234567
+        mino_two = arr[i-2] % 1234567
+        
+
+        arr.push(mino_one + mino_two)
+    }
+    
+    return (arr[n-1] + arr[n-2]) % 1234567;
+}
+
+
+// 다른 사람 풀이
+function solution(n) {
+    let a = 0, b = 1, f = 1;
+    for (let i = 2; i <= n; i++) {
+        f = (a + b) % 1234567
+        a = b % 1234567
+        b = f % 1234567
+    }
+    return f;
+  }
+  
