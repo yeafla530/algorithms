745c0c4ef8491f340e739eaa0b8d9c236019cfd1
yeafla530 <yeafla530@naver.com>
2023-03-08 00:42:47 +0900

    사각형 채우기3 - 난이도 상

--- 코드트리/IL/DP/사각형채우기3.py
+++ 코드트리/IL/DP/사각형채우기3.py
@@ -1,13 +1,16 @@
-MAX_INT = 1001
+# 너무 어려운듯
+MAX_INT = 1000
+MOD = 1000000007
 n = int(input())
+dp = [0] * (MAX_INT + 1)
 
-dp = [0] * (MAX_INT)
+dp[0] = 1
+dp[1] = 2
 
-dp[1] = 1
-dp[2] = 3
+for i in range(2, n+1):
+    dp[i] = (2 * dp[i-1] + 3 * dp[i-2]) % MOD
+    for j in range(i-3, -1, -1):
+        dp[i] = (dp[i] + dp[j] * 2) % MOD
 
-if n >= 3:
-    for i in range(3, n+1):
-        dp[i] = (dp[i-1] + 2 * dp[i-2]) % 10007
 
 print(dp[n])
