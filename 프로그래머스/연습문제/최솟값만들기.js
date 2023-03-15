ca601849aa90a50718531ce1a7197d6e5f238c0f
yeafla530 <yeafla530@naver.com>
2023-03-15 22:02:41 +0900

    최솟값 만들기 추가

--- 프로그래머스/연습문제/최솟값만들기.js
+++ 프로그래머스/연습문제/최솟값만들기.js
@@ -10,3 +10,17 @@ function solution(A,B){
 
     return answer;
 }
+
+// reduce
+function solution(A,B){
+    var answer = 0;
+    A.sort((a, b) => a - b)
+    B.sort((a, b) => b - a)
+    
+    answer = A.reduce((total, cur, idx) => total + cur * B[idx], 0)
+      
+    
+    
+
+    return answer;
+}
