7530dcbb251007db6ed2b60adf4de5ce9d8ef905
yeafla530 <yeafla530@naver.com>
2023-03-16 19:33:37 +0900

    큰수 만들기- 다시

---
+++ 프로그래머스/연습문제/큰수만들기.js
@@ -0,0 +1,15 @@
+function solution(number, k) {
+    const arr = []
+    
+    for (let i = 0; i < number.length; i++) {
+        while (arr.length > 0 && arr[arr.length-1] < number[i] && k > 0) {
+            k --;
+            arr.pop()
+        }
+        arr.push(number[i]);    
+        console.log(arr)
+    }
+    
+    arr.splice(number.length - k, k);
+    return arr.join("");
+}
