ac4f8e2e5cdc6b38a3266db6487af01982529307
yeafla530 <yeafla530@naver.com>
2023-03-27 01:23:38 +0900

    K번 최대값으로 이동하기

--- 코드트리/IL/BFS/K번최댓값으로이동하기.py
+++ 코드트리/IL/BFS/K번최댓값으로이동하기.py
@@ -1,3 +1,79 @@
+# 내 풀이
+# 1. 시작위치에서 출발해 인접한 숫자들 중 적혀있는 숫자가 x보다 작은 곳으로 전부 이동가능
+# 2. 적혀있는 수들 중 최댓값으로 이동
+# 3. 수가 여러개일 경우 행번호가 가장 작은곳으로 
+# 4. 행번호도 같으면 열번호가 가장 작은곳으로 이동
+
+
+# k번 반복한 후의 위치
+# k번 반복하지 못해도 이동할 위치 없으면 멈춤
+from collections import deque
+n, k = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+r, c = map(int, input().split())
+r -= 1
+c -= 1
+
+check = [[0]*n for _ in range(n)]
+num = grid[r][c]
+q = deque()
+
+dxs = [-1, 1, 0, 0]
+dys = [0, 0, 1, -1]
+
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def bfs():
+    global num
+    while len(q):
+        x, y = q.popleft()
+
+        for dx, dy in zip(dxs, dys):
+            nx = x + dx
+            ny = y + dy
+
+            if in_range(nx, ny) and grid[nx][ny] < num and not check[nx][ny]:
+                q.append((nx, ny))
+                check[nx][ny] = 1
+
+
+def find_next_pos(n):
+    global num, r, c
+    max_num = 0
+    # print(check)
+    for i in range(n-1, -1, -1):
+        for j in range(n-1, -1, -1):
+            if check[i][j] and max_num <= grid[i][j] and grid[i][j] != num:
+                max_num = grid[i][j]
+                # if n != k-1:
+                r, c = i, j
+    
+    num = max_num
+    # print(num)
+
+def clear_check():
+    for i in range(n):
+        for j in range(n):
+            check[i][j] = 0
+
+for i in range(k):
+    clear_check()
+    q.append((r, c))
+    # print(r, c)
+    check[r][c] = 1
+    bfs()
+    find_next_pos(n)
+
+
+print(r+1, c+1)
+
+
+
+
+
+
 import collections
 
 NOT_EXISTS = (-1, -1)
@@ -116,3 +192,94 @@ for _ in range(k):
 
 final_x, final_y = curr_cell
 print(final_x + 1, final_y + 1)
+
+
+# 내 풀이 
+from collections import deque
+
+n, k = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+r, c = map(int, input().split())
+visited = [[0]*n for _ in range(n)]
+NOT_EXISTS = (-1, -1)
+curr_cell = (r-1, c-1)
+
+dxs = [1, -1, 0, 0]
+dys = [0, 0, 1, -1]
+
+q = deque()
+
+def initial_visited():
+    for i in range(n):
+        for j in range(n):
+            visited[i][j] = 0
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def can_go(x, y, target_num):
+    return in_range(x, y) and not visited[x][y] and graph[x][y] < target_num
+
+def bfs():
+    curr_x, curr_y = curr_cell
+    visited[curr_x][curr_y] = True
+    q.append(curr_cell)
+    
+    target_num = graph[curr_x][curr_y]
+    
+    # BFS 탐색을 수행합니다.
+    while q:
+        curr_x, curr_y = q.popleft()
+        
+        for dx, dy in zip(dxs, dys):
+            new_x, new_y = curr_x + dx, curr_y + dy
+            
+            if can_go(new_x, new_y, target_num):
+                q.append((new_x, new_y))
+                visited[new_x][new_y] = True
+
+
+def need_update(best_pos, new_pos):
+    if best_pos == NOT_EXISTS:
+        return True
+
+    best_x, best_y = best_pos
+    new_x, new_y = new_pos
+
+    return (graph[new_x][new_y], -new_x, -new_y) > (graph[best_x][best_y], -best_x, -best_y)    
+
+
+def move():
+    global curr_cell
+
+    initial_visited()
+
+    bfs()
+
+    best_pos = NOT_EXISTS
+
+    for i in range(n):
+        for j in range(n):
+            if not visited[i][j] or (i, j) == curr_cell:
+                continue
+            
+            new_pos = (i, j)
+            if need_update(best_pos, new_pos):
+                best_pos = new_pos
+            
+
+    if best_pos == NOT_EXISTS:
+        return False
+    else:
+        curr_cell = best_pos
+        return True
+
+
+for _ in range(k):
+    is_moved = move()
+
+    if not is_moved:
+        break
+
+final_x, final_y = curr_cell
+print(final_x+1, final_y+1)
