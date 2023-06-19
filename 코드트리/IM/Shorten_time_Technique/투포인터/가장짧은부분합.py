b9cf3a0843f4b24c2511f969ba3aaee5e3d4b42d
yeafla530 <yeafla530@naver.com>
2023-06-19 21:20:38 +0900

    가장짧은 부분합

--- 코드트리/IM/Shorten_time_Technique/투포인터/가장짧은부분합.py
+++ 코드트리/IM/Shorten_time_Technique/투포인터/가장짧은부분합.py
@@ -1,20 +1,29 @@
 import sys
+# 어렵다
 INT_MAX = sys.maxsize
+
 n, s = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
 
 ans = INT_MAX
+
 sum_val = 0
 j = 0
+
 for i in range(1, n+1):
-    while j+1 <= n and sum_val < s:
+    while j + 1 <= n and sum_val < s:
         sum_val += arr[j+1]
         j += 1
 
     if sum_val < s:
         break
+
     ans = min(ans, j - i + 1)
+    
     sum_val -= arr[i]
+
 if ans == INT_MAX:
     ans = -1
+
+
 print(ans)
