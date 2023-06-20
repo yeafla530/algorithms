ef82bf863d879c8d2ff027e56baebfc81efa3f38
yeafla530 <yeafla530@naver.com>
2023-06-20 23:24:12 +0900

    수정

--- 해몽/0609_wootaecam/2.py
+++ 해몽/0609_wootaecam/2.py
@@ -9,21 +9,22 @@ for _ in range(k):
     diag[r-1][c-1] = True
 
 dy = [[0 for _ in range(m+1)] for _ in range(n+1)] # dy[r][c] := r행 c열인 직사각형을 최단거리로 이동하는 경우의 수
-dy[0][0] = 1
+dy[n][0] = 1
 
 # 각 대각선을 사용하지 않는 경우
-for r in range(n+1):
-    for c in range(m+1):
-        if r == 0 and c == 0:
+for r in range(n, -1, -1):
+    for c in range(0, m+1):
+        print(r, c)
+        if r == n and c == 0:
             continue
         
-        if c == 0 or r == 0:
+        if c == 0 or r == n:
             dy[r][c] = 1
 
         else:
-            dy[r][c] = (dy[r-1][c] + dy[r][c-1]) % MOD
+            dy[r][c] = (dy[r + 1][c] + dy[r][c-1]) % MOD
 
-
+print(dy)
 ans = 0
 for r in range(n):
     for c in range(m):
