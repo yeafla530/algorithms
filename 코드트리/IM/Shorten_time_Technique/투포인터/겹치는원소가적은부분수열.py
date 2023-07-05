1476a0af268bb26c95725f14974cd114adcf1e50
yeafla530 <yeafla530@naver.com>
2023-07-06 00:28:25 +0900

    [*] 0705 겹치는 원소가 적은 부분수열 - 정답코드 추가

--- 코드트리/IM/Shorten_time_Technique/투포인터/겹치는원소가적은부분수열.py
+++ 코드트리/IM/Shorten_time_Technique/투포인터/겹치는원소가적은부분수열.py
@@ -20,3 +20,29 @@ for i in range(1, n+1):
 
     # print(i, j)
 print(ans)
+
+
+# 정답 풀이 
+# hash map의 default값을 0으로 정의해주는 default dict
+from collections import defaultdict
+
+n, k = map(int, input().split())
+arr = [0] + list(map(int, input().split()))
+
+# 같은 원소가 k개 이하로 들어있는
+# 가장 긴 연속 부분 수열의 길이
+j = 0
+dict = defaultdict(lambda: 0)
+ans = 0
+for i in range(1, n+1):
+    while j + 1 <= n and dict[arr[j+1]] + 1 <= k:
+        j += 1
+        dict[arr[j]] = dict[arr[j]] + 1
+    ans = max(ans, j - i + 1)
+
+    dict[arr[i]] = dict[arr[i]] - 1
+    # if j + 1 <= n and i > j:
+    #     j = i
+
+    # print(i, j)
+print(ans)
