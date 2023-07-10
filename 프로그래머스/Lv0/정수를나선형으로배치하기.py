2f4f270b7f7b61b87748ae4b729f0cbe99e5d215
yeafla530 <yeafla530@naver.com>
2023-07-10 22:38:00 +0900

    [*] 0710 달팽이 문제 정석 추가

--- 프로그래머스/Lv0/정수를나선형으로배치하기.py
+++ 프로그래머스/Lv0/정수를나선형으로배치하기.py
@@ -33,3 +33,31 @@ def solution(n):
     #         print(answer[i][j], end=" ")
     #     print()
     return answer
+
+
+##### 달팽이 문제 정석
+def solution(n):
+    
+    dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+    x, y = 0, 0
+    dir_num = 0 # 방향 전환
+    answer = [[0 for _ in range(n)] for _ in range(n)]
+    
+    answer[x][y] = 1
+    
+    for i in range(2, n*n+1):
+        nx = x + dxs[dir_num]
+        ny = y + dys[dir_num]
+        
+        if not (0 <= nx < n and 0 <= ny < n) or answer[nx][ny] != 0:
+            dir_num = (dir_num + 1) % 4
+        
+        
+        x = x + dxs[dir_num]
+        y = y + dys[dir_num]
+    
+        answer[x][y] = i
+    
+    
+    
+    return answer
