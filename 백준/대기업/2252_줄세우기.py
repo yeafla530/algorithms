8cbb0b09e32f4c8984155c87eba38b7dfdfa3f44
yeafla530 <yeafla530@naver.com>
2023-07-21 00:10:05 +0900

    [+] 0720 줄세우기

---
+++ 백준/대기업/2252_줄세우기.py
@@ -0,0 +1,28 @@
+import heapq
+
+# 두학생의 키를 비교
+n, m = map(int, input().split())
+edges = [[] for _ in range(n+1)]
+indegree = [0] * (n+1)
+pq = []
+
+for i in range(m):
+    x, y = map(int, input().split())
+    edges[x].append(y)
+    indegree[y] += 1
+
+
+for i in range(1, n+1):
+    if not indegree[i]:
+        heapq.heappush(pq, i)
+
+while pq:
+    x = heapq.heappop(pq)
+    print(x, end=" ")
+    for y in edges[x]:
+        indegree[y] -= 1
+
+        if not indegree[y]:
+            heapq.heappush(pq, y)
+
+
--- 백준/대기업/줄세우기.py
+++
@@ -1,5 +0,0 @@
-# 최소 옮기기
-n = int(input())
-arr = list(int(input()) for _ in range(n))
-
-# 감도 안잡힘
