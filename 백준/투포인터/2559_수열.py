e204fbff9d0dbcb6d1257e6dd212b9c5c14ee266
yeafla530 <yeafla530@naver.com>
2023-07-28 23:55:52 +0900

    [*] 0728 수열

--- 백준/투포인터/2559_수열.py
+++ 백준/투포인터/2559_수열.py
@@ -1,9 +1,9 @@
 # O(n^2)안됨 
 n, k = map(int, input().split())
-arr = list(map(int, input().split()))
+arr = [0] + list(map(int, input().split()))
 # 누적합
 prefix_sum = [0] * (n-(k-1))
 
-for i in range(n):
-    
-
+j = 0
+for i in range(1, n):
+    print(i)
