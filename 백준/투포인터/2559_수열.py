9da6376b3b8f2c20bc7961e7f8b5c4cfc8cf676f
yeafla530 <yeafla530@naver.com>
2023-07-30 01:10:01 +0900

    [*] 0730 백준 수열

--- 백준/투포인터/2559_수열.py
+++ 백준/투포인터/2559_수열.py
@@ -1,9 +1,24 @@
+import sys
 # O(n^2)안됨 
 n, k = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
-# 누적합
-prefix_sum = [0] * (n-(k-1))
 
 j = 0
-for i in range(1, n):
-    print(i)
+ans = -sys.maxsize
+s = 0
+cnt = 0
+if k == 1:
+    print(max(arr))
+else:
+    for i in range(1, n-k+2):
+        while j+1 <= n and cnt != k:
+            cnt += 1
+            j += 1
+            s += arr[j]
+        
+        if cnt == k:
+            cnt -= 1
+            ans = max(ans, s)
+            s -= arr[i]
+
+    print(ans)
