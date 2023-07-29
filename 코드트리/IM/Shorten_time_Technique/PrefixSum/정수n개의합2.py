7cd8d85184ff4fcf77cebafeaa058b36ea16fe10
yeafla530 <yeafla530@naver.com>
2023-07-30 01:48:29 +0900

    [+] 0730 누적합

--- 코드트리/IM/Shorten_time_Technique/PrefixSum/정수n개의합2.py
+++ 코드트리/IM/Shorten_time_Technique/PrefixSum/정수n개의합2.py
@@ -1,14 +1,36 @@
-n, m = map(int, input().split())
-a = list(map(int, input().split()))
+n, k = map(int, input().split())
+arr = [0] + list(map(int, input().split()))
+prefix_sum = [0] * (n+1)
+ans = -987654321
 
-prefix = [0] * n
+for i in range(1, n+1):
+    prefix_sum[i] = prefix_sum[i-1] + arr[i]
 
-prefix[0] = a[0]
+for i in range(n-k+1):
+    ans = max(ans, prefix_sum[i+k] - prefix_sum[i])
 
-for i in range(1, n):
-    prefix[i] = prefix[i-1] + a[i]
+print(ans)
 
-result = 0
-for i in range(n-m):
-    result = max(result, prefix[i+m] - prefix[i])
-print(result)
+## 정답 풀이
+import sys
+
+
+INT_MIN = -sys.maxsize
+
+n, k = tuple(map(int, input().split()))
+arr = [0] + list(map(int, input().split()))
+prefix_sum = [0] * (n + 1)
+ans = INT_MIN
+
+def get_sum(s, e):
+    return prefix_sum[e] - prefix_sum[s-1]
+
+
+prefix_sum[0] = 0
+for i in range(1, n+1):
+    prefix_sum[i] = prefix_sum[i-1] + arr[i]
+
+for i in range(1, n-k+2):
+    ans = max(ans, get_sum(i, i+k-1))
+
+print(ans)
