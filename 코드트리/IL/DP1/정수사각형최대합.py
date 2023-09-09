b49393cd38b20c3b7a4a42f622f242b0a7b57094
yeafla530 <yeafla530@naver.com>
2023-09-09 19:26:35 +0900

    [+] 230909 정수사각형 최대합

--- 코드트리/IL/DP1/정수사각형최대합.py
+++ 코드트리/IL/DP1/정수사각형최대합.py
@@ -16,3 +16,26 @@ for i in range(1, n):
         dp[i][j] = max(dp[i-1][j] + grid[i][j], dp[i][j-1] + grid[i][j])
 
 print(dp[n-1][n-1])
+
+# 230909
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [[0]*n for _ in range(n)]
+
+def initial():
+    dp[0][0] = arr[0][0]
+
+    for i in range(1, n):
+        dp[0][i] = dp[0][i-1] + arr[0][i]
+        dp[i][0] = dp[i-1][0] + arr[i][0]
+
+
+initial()
+
+
+for i in range(1, n):
+    for j in range(1, n):
+        dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + arr[i][j]
+
+print(dp[n-1][n-1])
--- 코드트리/IL/DP1/정수사각형최소합.py
+++ 코드트리/IL/DP1/정수사각형최소합.py
@@ -21,22 +21,22 @@ print(dp[n-1][0])
 # 230909
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
-
 dp = [[0]*n for _ in range(n)]
 
 def initial():
-    dp[0][0] = arr[0][0]
+    dp[0][n-1] = arr[0][n-1]
 
+    for i in range(n-2, -1, -1):
+        dp[0][i] = dp[0][i+1] + arr[0][i]
+    
     for i in range(1, n):
-        dp[0][i] = dp[0][i-1] + arr[0][i]
-        dp[i][0] = dp[i-1][0] + arr[i][0]
-
+        dp[i][n-1] = dp[i-1][n-1] + arr[i][n-1]
 
 initial()
 
 
 for i in range(1, n):
-    for j in range(1, n):
-        dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + arr[i][j]
+    for j in range(n-2, -1, -1):
+        dp[i][j] = min(dp[i-1][j], dp[i][j+1]) + arr[i][j]
 
-print(dp[n-1][n-1])
+print(dp[n-1][0])
